b2c7ec20b3008e9eda2ce261cc833307abae2f5d
edumgoulart <edugoulart22@gmail.com>
2025-09-23 20:11:39 -0300

    ex01 tá funcionando

--- Exercicios_Programacao/ex01.c
+++ Exercicios_Programacao/ex01.c
@@ -11,7 +11,7 @@ void RemoverNome( void );
 void ListarNomes( void );
 
 int numNomes = 0;
-char *p;
+char *nomes;
 int tamTotalNomes = 1; //Contar o tamanho total da string de nomes, número de caracteres + \0
 
 int main(){
@@ -44,6 +44,7 @@ void Menu( void ){
         ListarNomes();
         break;
     case 4:
+        free(nomes);
         exit( 0 );
         break;
     default:
@@ -59,16 +60,24 @@ void AdicionarNome( void ){
     numNomes++;
     int tamNome = strlen( nome );
     tamTotalNomes += tamNome;
-    p = ( char * ) realloc( p, tamTotalNomes * sizeof(char) );
-    strcat( nome, " " );
-    strcat( p, nome );
-    printf( "%s\n", p );
-    printf("%ld %ld", strlen(p), sizeof(p)); //Encontrar outra forma de ver o tamanho de uma alocação dinamica
+    nomes = ( char * ) realloc( nomes, tamTotalNomes * sizeof(char) );
+    strcat( nome, " " ); //Um Byte a mais desnecessário no fim para o espaço
+    strcat( nomes, nome );
 }
 
 void RemoverNome( void ){
-
+    char nome[50];
+    char *ponteiroNomeInicio, *ponteiroNomeFim;
+    int tamNomesAteFim;
+    printf( "Digite o nome que você deseja remover:\n" );
+    scanf( "%s", nome );
+    ponteiroNomeInicio = strstr( nomes, nome );
+    ponteiroNomeFim = ponteiroNomeInicio + strlen(nome) *  sizeof(char) + 1;
+    tamNomesAteFim = strlen(ponteiroNomeFim);
+    memmove(ponteiroNomeInicio, ponteiroNomeFim, tamNomesAteFim + 1);
+    nomes = ( char * ) realloc( nomes, (strlen(nomes) - strlen(nome) - 1) * sizeof(char) );
 }
-void ListarNomes( void ){
 
+void ListarNomes( void ){
+    printf( "%s\n", nomes );
 }
