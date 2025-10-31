747cec07d6af3d3709bcfa6b8b090a885541645a
edumgoulart <edugoulart22@gmail.com>
2025-10-31 16:24:50 -0300

    Trabalho_Buffer: BuscarPessoa() adicionado

--- Trabalho_Buffer/main.c
+++ Trabalho_Buffer/main.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-#define TAM_VARIAVEIS ( 2 * sizeof( int ) )
-#define TAM_NOME ( 50 * sizeof( char ) )
+//Tamanho dos espaços que vão ser alocados na memória
+#define TAM_VARIAVEIS ( 3 * sizeof( int ) + TAM_NOME )
+#define TAM_NOME ( 50 * sizeof( char ) ) //Alterar manualmente os scanf, caso mude o valor
 #define TAM_IDADE ( sizeof(int) )
 #define TAM_EMAIL ( 50 * sizeof( char ) )
-#define INT_1 ( *( int * )pBuffer )
-#define NUM_PESSOAS ( *( int * )( pBuffer + sizeof( int ) ) )
 
-// PBuffer =  INT_1   | NUM_PESSOAS  | valoresPessoas( Nome , Idade , Email )
-//  sizeof     int         int                      50*char + int  + 50*char
+//Ponteiros para espaços na memórias prontos para usar
+#define INT_1 ( *( int * )pBuffer )
+#define INT_2 ( *( int * )( pBuffer + sizeof( int ) ) )
+#define NUM_PESSOAS ( *( int * )( pBuffer + 2 * sizeof( int ) ) )
+#define NOME_PESSOA ( ( char * )( pBuffer + 3 * sizeof( int ) ) )
+
+// PBuffer =  INT_1 |   INT_2   | NUM_PESSOAS  | NOME_PESSOA | valoresPessoas( Nome , Idade , Email )
+//  sizeof     int       int         int           50*char            50*char + int  + 50*char
 
 void * Menu( void * pBuffer );
 void * AdicionarPessoa( void * pBuffer );
@@ -19,10 +25,9 @@ void ListarTodos( void * pBuffer );
 
 int main(){
 
-    //pBuffer -> operação
+    //pBuffer -> Única memória do programa
     void * pBuffer = malloc( TAM_VARIAVEIS );
 
-    //numPessoas = 0
     NUM_PESSOAS = 0;
 
     while(1){
@@ -32,6 +37,7 @@ int main(){
 
 void * Menu( void * pBuffer ){
     
+    printf( "MENU:\n" );
     printf( "1. Adicionar Pessoa\n" );
     printf( "2. Remover Pessoa\n" );
     printf( "3. Buscar Pessoa\n" );
@@ -65,7 +71,7 @@ void * Menu( void * pBuffer ){
 }
 
 void * AdicionarPessoa( void * pBuffer ){
-    // numPessoas++
+
      NUM_PESSOAS++;
 
     // Adiciona espaço para uma nova pessoa no Buffer
@@ -74,17 +80,16 @@ void * AdicionarPessoa( void * pBuffer ){
     // pessoaAtual vai apontar para o novo espaço para pessoa no final da lista
     void * pessoaAtual = ( pBuffer + TAM_VARIAVEIS + ( NUM_PESSOAS - 1 ) * ( TAM_EMAIL + TAM_NOME + TAM_IDADE ) );
 
+    // Guarda os dados da pessoa em seus respectivos lugares no espaço a recém criado
     printf( "Digite o nome da pessoa: " );
-    // Guarda o nome no Buffer
-    scanf("%s", ( char * )pessoaAtual);
+    scanf(" %49[^\n]", ( char * )pessoaAtual);
 
     printf( "Digite a idade da pessoa: " );
-    // Guarda idade após o nome
     scanf("%d", ( int * )( pessoaAtual + TAM_NOME ) );
 
     printf( "Digite o email da pessoa: " );
-    // Guarda email após o nome e idade
-    scanf( "%s", ( char * )( pessoaAtual + TAM_NOME + TAM_IDADE ) );
+    scanf( " %49[^\n]", ( char * )( pessoaAtual + TAM_NOME + TAM_IDADE ) );
+    printf("\n");
 
     return pBuffer;
 
@@ -95,12 +100,28 @@ void RemoverPessoa( void * pBuffer ){
 }
 
 void BuscarPessoa( void * pBuffer ){
-    //Busca pelo Nome se tem mais de 1 nome igual, retorna todos com seus respectivos indices
+
+    printf( "Digite o nome da pessoa: " );
+    scanf( " %49[^\n]", NOME_PESSOA );
+
+    for( INT_1 = 0 ; INT_1 < NUM_PESSOAS ; INT_1++ ){
+
+        void * pessoaAtual = ( pBuffer + TAM_VARIAVEIS + INT_1 * ( TAM_NOME + TAM_IDADE + TAM_EMAIL ) );
+        
+        if( strstr( pessoaAtual, NOME_PESSOA ) ){
+            printf( "%d.\n", INT_1 );
+            printf( " Nome: %s\n" , (char *)pessoaAtual );
+            printf( " Idade: %d\n" , *(int *)( pessoaAtual + TAM_NOME ) );
+            printf( " Email: %s\n\n" , (char *)( pessoaAtual + TAM_NOME + TAM_IDADE ) );
+        }
+       
+    }
 }
 
 void ListarTodos( void * pBuffer ){
 
     if( NUM_PESSOAS == 0 ) printf("Nenhuma Pessoa adicionada a lista\n\n");
+
     for( INT_1 = 0 ; INT_1 < NUM_PESSOAS ; INT_1++ ){
 
         void * pessoaAtual = ( pBuffer + TAM_VARIAVEIS + INT_1 * ( TAM_NOME + TAM_IDADE + TAM_EMAIL ) );
